6d805bd9bf5a6735d7ee4f74d350e15fd2c8f32c
Brijesh Bittu <brijeshb42@gmail.com>
2015-07-07 15:29:16 +0530

    Update tasks.py
    Send mail once a day

--- tasks.py
+++ tasks.py
@@ -5,6 +5,6 @@ from scrape import get_data_send_mail
 queue = RedisHuey("scrape-trending")
 
 
-@queue.periodic_task(crontab(minute="0", hour="*/12"))
+@queue.periodic_task(crontab(minute="0", hour="*"))
 def send_mail():
     get_data_send_mail()
